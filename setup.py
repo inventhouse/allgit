f1852c8a5d9590adaa37dbf181f4816c790331a0
Ben Holt <bholt+github@edx.org>
2021-02-10 17:07:21 -0500

    initial start of a setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2021 Benjamin Holt -- MIT License
+
+from setuptools import setup
+
+from allgit import _version
+
+setup(
+    name="allgit",
+    version=_version,
+    description="""Powerful "git multiplexer" for easily working with many repositories""",
+    long_description="FIXME: See readme for now",
+    url="https://github.com/inventhouse/allgit",
+    author="Benjamin Holt",
+    license="MIT",
+    py_modules=["allgit"],
+    entry_points={
+        "console_scripts": [
+            "allgit=allgit:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: Unix',
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        'Topic :: Software Development :: Version Control',
+    ],
+    keywords="git",
+)
